7ea212f296b22d28c0d2f5aa20d998d9788d1347
Karly Mareka <karly.mareka@gmail.com>
2019-07-16 21:34:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,8 @@ class ApplicationController < Sinatra::Base
     binding.pry 
     @article.update(title: params[:title], content: params[:content])
     @article.save
-    redirect "/articles/#{@article.id}"
+    #redirect "/articles/#{@article.id}"
+    erb :edit 
     
   end
   
