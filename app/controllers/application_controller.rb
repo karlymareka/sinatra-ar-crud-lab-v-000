181be9549ce255b9ed2d07a2c2a8555272e0d7e1
Karly Mareka <karly.mareka@gmail.com>
2019-07-16 21:45:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
   
   get '/articles/:id/edit' do
     @article = Article.find_by(params[:id])
-    erb :edit
+    #erb :edit
     redirect "/articles/#{@article.id}"
   end 
   
