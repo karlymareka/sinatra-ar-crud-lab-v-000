82178116c847290f6402ee4e867e4f03f581843f
Karly Mareka <karly.mareka@gmail.com>
2019-07-16 22:07:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,6 +52,7 @@ class ApplicationController < Sinatra::Base
 =end 
  
   delete '/articles/:id/delete' do
+    binding.pry 
      @article = Article.find_by(params[:id])
      @article.destroy
      redirect "/articles/#{@article.id}"
